cb024e8e1375f26772e157893f05de7babbac605
Bernhard Schuster <bernhard@ahoi.io>
2017-08-30 07:28:43 +0200

    refactor/pipeline: split containers from juice core pipeline

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,44 @@ extern crate askama;
 use askama::Template;
 
 #[derive(Template)]
-#[template(path = "concourse.yml")]
-struct ConcourseYml<'a> {
+#[template(path = "juice.yml")]
+struct JuiceYml<'a> {
+    operatingsystems: &'a Vec<String>,
+    backends: &'a Vec<String>
+}
+
+#[derive(Template)]
+#[template(path = "juice-containers.yml")]
+struct ContainerYml<'a> {
     operatingsystems: &'a Vec<String>,
     backends: &'a Vec<String>
 }
 
 
 fn main() {
-    // TODO read this from a `matrix.yml` file
+    // TODO read this from a `matrix.yml` file using yml-rust
     let operatingsystems = &vec!["fedora".to_string(),"ubuntu".to_string()];
     let backends = &vec!["default".to_string(),"native".to_string(),"cuda".to_string(),"opencl".to_string()];
 
-    let config = ConcourseYml {
+    let juice = JuiceYml {
+         operatingsystems: operatingsystems,
+         backends: backends,
+         };
+    let containers = ContainerYml {
          operatingsystems: operatingsystems,
          backends: backends,
          };
 
-    let mut file = OpenOptions::new()
+    let mut f_juice = OpenOptions::new()
                             .write(true)
                             .create(true)
                             .truncate(true)
-                            .open("concourse.yml").unwrap();
-
-    {
-        println!("{}", config.render());
-    }
-    file.write_all(config.render().as_str().as_bytes()).unwrap();
+                            .open("juice.yml").unwrap();
+    f_juice.write_all(juice.render().unwrap().as_str().as_bytes()).unwrap();
+    let mut f_containers = OpenOptions::new()
+                            .write(true)
+                            .create(true)
+                            .truncate(true)
+                            .open("juice-containers.yml").unwrap();
+    f_containers.write_all(containers.render().unwrap().as_str().as_bytes()).unwrap();
 }
