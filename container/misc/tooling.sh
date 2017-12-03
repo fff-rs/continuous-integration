cc111629d011ea888f0e6fcb569f20f582c98d2c
Bernhard Schuster <bernhard@ahoi.io>
2017-12-03 20:08:13 +0100

    feat/bump: update to micro 1.3.4

--- container/misc/tooling.sh
+++ container/misc/tooling.sh
@@ -3,12 +3,12 @@ set -e
 
 cd /usr/local/bin/
 url='https://github.com/rust-lang-nursery/mdBook/releases/download/0.0.21/mdBook-0.0.21-x86_64-unknown-linux-gnu.tar.gz'
-curl -L ${url} | tar xvz
+curl -L ${url} | tar -Uxvz
 cd -
 
 cd /tmp
 url='https://github.com/zyedidia/micro/releases/download/v1.3.4/micro-1.3.4-linux64.tar.gz'
-curl -L ${url} | tar xvz
+curl -L ${url} | tar -Uxvz
 cp -vf ./micro-*/micro /usr/local/bin/
 cd -
 
