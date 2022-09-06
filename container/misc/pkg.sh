65c9522eb87fb923e380dcb13cf003c8a347d559
Bernhard Schuster <bernhard@ahoi.io>
2022-09-06 12:03:49 +0200

    rename all Dockerfiles to Containerfile, remove openssl-static, doesn't exist anymore

--- container/misc/pkg.sh
+++ container/misc/pkg.sh
@@ -4,7 +4,7 @@ dnf install -y dnf-plugins-core \
 	&& dnf config-manager -y --add-repo=https://negativo17.org/repos/fedora-nvidia.repo \
 	&& dnf install -y curl clang make cmake git \
 			openblas-static openblas-openmp openblas-devel \
-			openssl-static openssl-libs openssl-devel \
+			openssl-libs openssl-devel \
 			capnproto capnproto-libs capnproto-devel \
 			cuda-cudnn \
 			cuda-cudnn-devel \
